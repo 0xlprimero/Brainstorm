8eea3500d88a1d6f9b7e0cbb14073b7e49f2820f
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-01 17:58:54 +0530

    Cleared out project

--- index.js
+++ index.js
@@ -2,5 +2,11 @@ var Peer = require('simple-peer')
 
 
 peer = new Peer({
-	intiator: location.hash === 
+	initiator: location.hash === '#init',
+	stream: true,
+	trickle: false
+})
+
+peer.on('signal', function(data) {
+	document.getElementById('myId') = JSON.stringify(data)
 })
