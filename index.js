d178aa6cba4aac02778e47c058fb2cb029f72d17
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-02 16:44:38 +0530

    Webkit navigator video working

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
-var Peer = require('simple-peer')
 
-var peer = new Peer({
-	initiator: location.pathname === '/init',
-	trickle: false
+navigator.webkitGetUserMedia({ video: true, audio: true}, function(stream) {
+	var Peer = require('simple-peer')
+
+	var peer = new Peer({
+		initiator: location.pathname === '/init',
+		trickle: false,
+		stream: stream
+	})
+
+	peer.on('signal', function(data) {
+		document.getElementById('myId').value = JSON.stringify(data)
+	})
+
+	document.getElementById('connect').addEventListener('click', function () {
+		var peerId = JSON.parse(document.getElementById('peerId').value)
+		peer.signal(peerId)
+	})
+
+	peer.on('stream', function(stream) {
+		var video = document.createElement('video')
+		document.body.appendChild(video)
+		video.src = window.URL.createObjectURL(stream)
+		video.play()
+	})
+}, function(err) {
+	console.error(err)
 })
 
-peer.on('signal', function(data) {
-	document.getElementById('myId').value = JSON.stringify(data)
-})
-
-document.getElementById('connect').addEventListener('click', function () {
-	var peerId = JSON.parse(document.getElementById('peerId').value)
-	peer.signal(peerId)
-})
