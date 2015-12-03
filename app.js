7be381150e72c695767fe64e8e3266a820b90041
Sarthak Munshi <sarthakmunshi@gmail.com>
2015-12-03 13:53:47 +0530

    Openshift enivironment variables added

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ var path = require('path')
 
 var app = express();
 
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080)
+app.set('ipaddr', process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1")
+
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
